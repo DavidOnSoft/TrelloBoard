dfc5e8c76823c65d255df96b5e812ad60dec3b87
David <dmartin@onsoft.fr>
2018-04-26 16:13:46 +0200

    toto

--- app/actions/TaskActionCreators.js
+++ app/actions/TaskActionCreators.js
@@ -1,15 +1,15 @@
 import AppDispatcher from '../AppDispatcher';
-import constants from '../constants';
-import TrelloAPI from './api/TrelloApi';
+import constants from '../stores/constants';
+import TrelloAPI from '../api/TrelloApi';
 
 let TaskActionsCreators = {
 
-        addTask(cardId, task) {
-            AppDispatcher.dispatchAsync(TrelloAPI.addTask(cardId, task), {
-                    request: constants.CREATE_TASK,
-                    success: constants.CREATE_TASK_SUCCESS,
-                    failure: constants.CREATE_TASK_ERROR
-                }, { cardId, task }
-            };
-        };
-        export default TaskActionsCreators;
+    addTask(cardId, task) {
+        AppDispatcher.dispatchAsync(TrelloAPI.addTask(cardId, task), {
+            request: constants.CREATE_TASK,
+            success: constants.CREATE_TASK_SUCCESS,
+            failure: constants.CREATE_TASK_ERROR
+        }, { cardId, task });
+    }
+}
+export default TaskActionsCreators;
